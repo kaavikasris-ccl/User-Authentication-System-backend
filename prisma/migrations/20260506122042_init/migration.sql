ec963e26bd01e3896886fb8cd151e91660e9c4cd
kaavikasris <kaavikasri.s@crystalcodelabs.com>
2026-05-13 15:15:14 +0530

    Commit

--- prisma/migrations/20260506122042_init/migration.sql
+++ prisma/migrations/20260506122042_init/migration.sql
@@ -1,20 +1,10 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `User`;
-
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
-    `otp` INTEGER NULL,
-    `otp_expiry` DATETIME(3) NULL,
-
+    `password` VARCHAR(191) NULL,
+    `otp` VARCHAR(191) NULL,
+    `otpExpiry` DATETIME(3) NULL,
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
